ff4d476f84bb6bfbcc549dae538b74466602a19c
yoona610 <xiaotengxinai@gmail.com>
2021-09-21 18:35:49 +0900

    Initial Commit

---
+++ src/Chapter0.java
@@ -0,0 +1,17 @@
+
+public class Chapter0 {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です！");
+		System.out.println("お酒が好きです");
+		System.out.println("31+31の計算をします！");
+		System.out.println(31 + 31);
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("こたえは" + 64);
+		int x;
+		x = 6;
+		System.out.println(x*x*3.14);
+	}
+}
---
+++ src/Chapter1.java
@@ -0,0 +1,19 @@
+
+public class Chapter1 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+		System.out.println(result);
+		System.out.println(favoriteCharacter);
+		System.out.println(pi);
+		System.out.println(number);
+		System.out.println(msg);
+	}
+}
