cfee24579b9407d6c3d8a616f5cb22787f3f2337
yoona610 <xiaotengxinai@gmail.com>
2021-09-23 17:03:54 +0900

    [ADD]4章:配列/練習問題

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -1,18 +1,18 @@
 
 public class Chapter3 {
 	public static void main(String[] args) {
-		System.out.print("数あてゲーム");
-		int ans = new java.util.Random().nextInt(10);
-		for (int i = 0; i < 5; i++) {
-			System.out.println("0から9の数字を入力してください");
-			int num = new java.util.Scanner(System.in).nextInt();
-			if (ans == num) {
-				System.out.println("アタリ");
-				break;
-			}else {
-				System.out.println("違います");
-			}
-		}
-		System.out.println("ゲームを終了します");
+//		System.out.print("数あてゲーム");
+//		int ans = new java.util.Random().nextInt(10);
+//		for (int i = 0; i < 5; i++) {
+//			System.out.println("0から9の数字を入力してください");
+//			int num = new java.util.Scanner(System.in).nextInt();
+//			if (ans == num) {
+//				System.out.println("アタリ");
+//				break;
+//			}else {
+//				System.out.println("違います");
+//			}
+//		}
+//		System.out.println("ゲームを終了します");
 	}
 }
