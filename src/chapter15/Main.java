f4544ed018564c958f1c5f4ab018481477a13164
yoona610 <xiaotengxinai@gmail.com>
2021-10-03 23:23:05 +0900

    [ADD]15章:文字列と日付の扱い/練習問題

---
+++ src/chapter15/Main.java
@@ -0,0 +1,39 @@
+package chapter15;
+
+public class Main {
+	public static void main(String[] args) {
+		/*String s1 = "スッキリJava";
+		String s2 = "Java";
+		String s3 = "java";
+		if (s2.equals(s3)) {
+			System.out.println("s2とs3は等しい");
+		}
+		if (s2.equalsIgnoreCase(s3)) {
+			System.out.println("s2とs3はケースを区別しなければ等しい");
+		}
+		System.out.println("s1の長さは" + s1.length() + "です");
+		if(s1.isEmpty()) {
+			System.out.println("s1は空文字です");
+		}*/
+
+		/*String s1 = "Java and JavaScript";
+		if(s1.contains("Java")) {
+			System.out.println("文字列s1は、Javaを含んでいます");
+		}
+		if(s1.endsWith("Java")) {
+			System.out.println("文字列s1は、Javaが末尾にあります");
+		}
+		System.out.println("文字列s1で最初にJavaが登場する位置は"+ s1.indexOf("Java"));
+		System.out.println("文字列s1で最後にJavaが登場する位置は"+ s1.lastIndexOf("Java"));*/
+
+		/*String s1 = "Java programming";
+		System.out.println("文字列のs1の4文字目以降は" + s1.substring(3));
+		System.out.println("文字列のs1の4～8文字目は" + s1.substring(3, 8));*/
+
+		/*StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 10000; i++) {
+			sb.append("Java");
+		}
+		String s = sb.toString();*/
+	}
+}
