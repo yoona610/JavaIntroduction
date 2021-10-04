37cd07d96767b7031e4c1228b743ef4efaed2ec9
yoona610 <xiaotengxinai@gmail.com>
2021-10-04 22:40:27 +0900

    [ADD]15章:文字列と日付の扱い/練習問題

--- src/chapter15/Main.java
+++ src/chapter15/Main.java
@@ -1,5 +1,8 @@
 package chapter15;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 	public static void main(String[] args) {
 		/*String s1 = "スッキリJava";
@@ -35,5 +38,111 @@ public class Main {
 			sb.append("Java");
 		}
 		String s = sb.toString();*/
+
+		/*String s = "abc,def:ghi";
+		String[] words = s.split("[,:]");
+		for (String w : words) {
+			System.out.println(w + "->");
+		}*/
+
+		/*String s = "abc,def:ghi";
+		String w = s.replaceAll("[beh]","X");
+		System.out.println(w);*/
+
+		/*final String FORMAT = "%-9s %-13s 所持金%,6d";
+		String s = String.format(FORMAT, hero.getjob(), hero.getGold());
+		System.out.println(s);*/
+
+		/*long start = System.currentTimeMillis();
+		long end = System.currentTimeMillis();
+		System.out.println("処理にかかった時間は・・・" + (end - start) + "ミリ秒でした");*/
+
+		/*Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1600705425827L);
+		System.out.println(past);*/
+
+		/*Calendar c = Calendar.getInstance();
+		c.set(2019,8,22,1,23,45);
+		c.set(Calendar.MONTH, 9);
+		Date d = c.getTime();
+		System.out.println(d);
+		Date now = new Date();
+		c.setTime(now);
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は" + y + "年です");*/
+
+		/*SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date d = f.parse("2020/09/22 01:23:45");
+		System.out.println(d);
+		Date now = new Date();
+		String s = f.format(now);
+		System.out.println("現在は" + s + "です");
+		*/
+
+		/*Instant i1 = Instant.now();
+		Instant i2 = Instant.ofEpochMilli(1600705425827L);
+		long l = i2.toEpochMilli();
+
+		ZonedDateTime z1 = ZonedDateTime.now();
+		ZonedDateTime z2 = ZonedDateTime.of(2020,1,2,3,4,5,6,ZoneId.of("Asia/Tokyo"));
+
+		Instant i3 = z2.toInstant();
+		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+
+		System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+		System.out.println("ロンドン:" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+		if(z2.isEqual(z3)){
+			System.out.println("これらは同じ瞬間を指しています");
+		}*/
+
+		/*LocalDateTime l1 = LocalDateTime.now();
+		LocalDateTime l2 = LocalDateTime.of(2020, 1, 1, 9, 5, 0, 0);
+		ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+		LocalDateTime l3 = z1.toLocalDateTime();
+		*/
+
+		/*DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate ldate = LocalDate.parse("2020/09/22", fmt);
+		LocalDate ldatep = ldate.plusDays(1000);
+		String str = ldatep.format(fmt);
+		System.out.println("1000日後は" + str);
+
+		LocalDate now = LocalDate.now();
+		if (now.isAfter(ldatep)) {
+			System.out.println("1000日後は過去日付です");
+		}*/
+
+		/*LocalDate d1 = LocalDate.of(2020, 1, 1);
+		LocalDate d2 = LocalDate.of(2020, 1, 4);
+
+		Period p1 = Period.ofDays(3);
+		Period p2 = Period.between(d1, d2);
+
+		LocalDate d3 = d2.plus(p2);*/
+
+		/*StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(i + 1).append(",");
+		}
+		String s = sb.toString();
+		String[] a = s.split(",");
+		System.out.println(s);*/
+
+		/*Date now = new Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(now);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		day += 100;
+		c.set(Calendar.DAY_OF_MONTH, day);
+		Date future = c.getTime();
+		SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
+		System.out.println(f.format(future));*/
+
+		LocalDate now = LocalDate.now();
+		LocalDate future = now.plusDays(100);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+		System.out.println(future.format(fmt));
 	}
 }
