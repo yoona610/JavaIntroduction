bd849ea6dd5735832258b269e8da3970f5173186
yoona610 <xiaotengxinai@gmail.com>
2021-10-10 23:33:48 +0900

    [ADD]18章:まだまだ広がるJavaの世界/練習問題

--- src/chapter18/Main.java
+++ src/chapter18/Main.java
@@ -1,15 +1,21 @@
 package chapter18;
 
-import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		FileReader fr = new FileReader("data.txt");
+		/*FileReader fr = new FileReader("data.txt");
 		int input = fr.read();
 		while (input != -1) {
 			System.out.print((char)input);
 			input = fr.read();
 		}
-		fr.close();
+		fr.close();*/
+		FileWriter fw = new FileWriter("data.txt");
+		fw.write('そ');
+		fw.write('れ');
+		fw.write('で');
+		fw.write('は');
+		fw.close();
 	}
 }
